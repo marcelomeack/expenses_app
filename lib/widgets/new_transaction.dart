11738a693f0a20cc43cda7627e787f556010b45c
marcelomeack <marcelomeack@gmail.com>
2021-07-07 14:10:36 -0300

    feat: add adaptive button for IOS and Android

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/cupertino.dart';
+import './adapative_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -85,16 +86,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           ? 'No Date Chosen!'
                           : 'Picked Date: ${DateFormat('d/MM/y').format(_selectedDate)}'),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text('Chose Date',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            onPressed: _presentDatePicker)
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text('Chose Date',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            onPressed: _presentDatePicker),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
